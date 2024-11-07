0fe52a1060e64b307cbdac4ab8d3bfc452f80a9a
eduardo <eduardosantana1313@gmail.com>
2024-11-07 18:09:21 -0300

    adding postgres db

--- app/src/test/kotlin/br/com/edu/observability/infra/rest/v1/PokeDeskControllerTest.kt
+++ app/src/test/kotlin/br/com/edu/observability/infra/rest/v1/PokeDeskControllerTest.kt
@@ -1,14 +1,19 @@
 package br.com.edu.observability.infra.rest.v1
 
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
@@ -50,6 +55,25 @@ class PokeDeskControllerTest {
         mvc.perform(get("/v1/pokemon")
             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
+    }
+
+    companion object {
+
+        private var postgresSqlContainer = PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine"))
+
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            postgresSqlContainer.start()
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun psqlProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url") { postgresSqlContainer.jdbcUrl }
+            registry.add("spring.datasource.username") { postgresSqlContainer.username }
+            registry.add("spring.datasource.password") { postgresSqlContainer.password }
+        }
     }
 
 
